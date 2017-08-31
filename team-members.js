77c9b85420d452dc3ef8f26fe0c076ba897a9a33
Umesh <umeshmeena141@gmail.com>
2017-09-01 00:14:07 +0530

    Team Page Added

---
+++ team-members.js
@@ -0,0 +1,25 @@
+var person=[];		
+		person[0]={
+			Name:"Umesh Meena",
+			Speciality: "",
+			Others: "",
+			img:"images/placeholder.png"
+		}
+		person[1]={
+			Name:"Umesh Meena",
+			Speciality: "",
+			Others: "",
+			img:"images/placeholder.png"
+		}
+		person[2]={
+			Name:"Umesh Meena",
+			Speciality: "",
+			Others: "",
+			img:"images/placeholder.png"
+		}
+		person[3]={
+			Name:"Umesh Meena",
+			Speciality: "",
+			Others: "",
+			img:"images/placeholder.png"
+		}
