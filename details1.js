5a9497bd281156e9d2f25a3bbbee3697dcb69b11
Umesh <umeshmeena141@gmail.com>
2017-09-02 00:53:25 +0530

    Team Page Updated2

--- details1.js
+++ details1.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 	$("img").on({mouseenter:function(){
 		$(this).animate({width:"400px",left:"-=1%",paddingTop:"-=1%",zIndex:"10"},300);
-		$('.overlay').css({"position":"absolute","left":"$(img).position().left","top":"$(img).position().top","zIndex":"20"});
+		$('.overlay').css({"position":"absolute","left":"9%","top":"22.5%","zIndex":"20"});
+		
 		
 	},
 	mouseleave:function(){
-		$(this).animate({width:"280px",left:"+=1%",paddingTop:"+=1%",zIndex:"1"},100);
+		$(this).animate({width:"26%",left:"+=1%",paddingTop:"+=1%",zIndex:"1"},100);
 		
 	}
 	});
