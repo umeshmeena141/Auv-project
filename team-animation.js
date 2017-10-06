19c38e328a93035e2c08251f37a4a1919f0cbaa3
Umesh <umeshmeena141@gmail.com>
2017-10-06 23:21:58 +0530

    Changes in Team page animation and link added to contact page

--- team-animation.js
+++ team-animation.js
@@ -4,8 +4,7 @@ function animate2(x,navhover){
 			return;
 		}
 		else{
-            $(navhover).css({ "color": "black" });
-            $(page[1]).css({ "color": "white" });
+           /* 
             $(page[0]).css({ "transform": "rotateY(180deg)", "transition": "all 0.8s linear" });
             //$(page[0]).fadeTo(600, '0.5');
             $(page[0]).delay(800).queue(function (next) {
@@ -13,7 +12,13 @@ function animate2(x,navhover){
                 $(x).css({ "display": "block" });
                 next();
             });
-            //$(x).fadeTo(800, '1');
+            //$(x).fadeTo(800, '1');*/
+            $(x).css({ "opacity": "1"});
+            $(navhover).css({ "color": "black" });
+            $(page[1]).css({ "color": "white" });
+            $(page[0]).slideUp(1000);
+            $(page[0]).css({ "opacity": "0.7", "transition": 'opacity 1s' });
+            $(x).slideDown(1000);
             page = [x, navhover];
         
 		}
