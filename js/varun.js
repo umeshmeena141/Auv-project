0113ef6a9b5a5a6b6087ec5743c4743cf3224e50
Karthic <klikesmath@yahoo.in>
2017-10-10 23:34:21 +0530

    added youtube link and changed varun's navbar size

--- js/varun.js
+++ js/varun.js
@@ -3,12 +3,12 @@
 			var x2 = document.getElementById("index1");
 			var x3 = document.getElementById("LOGO1");
 			if(window.innerWidth > 856) {
-				x1.style.top = "59px";
+				x1.style.top = "58px";
 				x2.style.display = "none";
 				x3.style.display="inline";
 			}
 			else {
-				x1.style.top = "43px";
+				x1.style.top = "42px";
 				x2.style.display = "inline";
 				x3.style.display = "none";
 			}
@@ -19,23 +19,32 @@
 					x1.style.top = "59px";
 					x2.style.display = "none";
 					x3.style.display="inline";
+					
 				}
 				else {
 					x1.style.top = "43px";
 					x2.style.display = "inline";
 					x3.style.display = "none";
 				}
+				if(window.innerWidth > 450) {
+					x1.style.zoom = "100%";
+				}
+				else {
+					x1.style.zoom = "90%";
+				}
 			}
 			$(document).ready(function() {
 				$("#part2").click(function() {
-					if(window.innerWidth > 450)
+					if(window.innerWidth > 450) {
 						$("body,html").animate(
 							{scrollTop :   $("#mechanical").offset().top - 90}, 400
 						);  //how fast the scrolling animation will be in miliseconds
-					else
+					}
+					else {
 						$("body,html").animate(
 							{scrollTop :   $("#mechanical").offset().top - 140}, 400
 						);  //how fast the scrolling animation will be in miliseconds
+					}
 				});
 			});
 			$(document).ready(function() {
