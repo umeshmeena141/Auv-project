bd6630439c8afde6a162d8712ae4a576a60491d8
Umesh <umeshmeena141@gmail.com>
2017-10-12 17:37:44 +0530

    height issue solved

--- team-animation.js
+++ team-animation.js
@@ -4,23 +4,22 @@ function animate2(x,navhover){
 			return;
 		}
 		else{
-           /* 
-            $(page[0]).css({ "transform": "rotateY(180deg)", "transition": "all 0.8s linear" });
-            //$(page[0]).fadeTo(600, '0.5');
-            $(page[0]).delay(800).queue(function (next) {
-                $(this).css({ "transform": "rotateY(0deg)", "transition": "all 0.1s linear", "display": "none" });
-                $(x).css({ "display": "block" });
-                next();
-            });
-            //$(x).fadeTo(800, '1');*/
+
             $(x).css({ "opacity": "1"});
+			$(page[0]).removeClass("active");
             $(navhover).css({ "color": "black" });
             $(page[1]).css({ "color": "white" });
             $(page[0]).slideUp(1000);
             $(page[0]).css({ "opacity": "0.7", "transition": 'opacity 1s' });
             $(x).slideDown(1000);
             page = [x, navhover];
-        
+			$(page[0]).addClass("active");
+			if($("#Software_Team").hasClass("active")){
+				$('.info').css({"height":"1100px"});
+			}
+			else{
+				$('.info').css({"height":"900px"});
+			}
 		}
 		
 		 
