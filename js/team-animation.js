09e19f4d50d4f0028b3fecf7776d971d66dc6851
Umesh <umeshmeena141@gmail.com>
2017-10-13 23:27:34 +0530

    Container's positioning change wrt footer

--- js/team-animation.js
+++ js/team-animation.js
@@ -6,49 +6,49 @@ function animate2(x,navhover){
 		else{
 
             $(x).css({ "opacity": "1"});
-			$(page[0]).removeClass("active");
+			//$(page[0]).removeClass("active");
             $(navhover).css({ "color": "black" });
             $(page[1]).css({ "color": "white" });
             $(page[0]).slideUp(1000);
             $(page[0]).css({ "opacity": "0.7", "transition": 'opacity 1s' });
             $(x).slideDown(1000);
             page = [x, navhover];
-			$(page[0]).addClass("active");
-			if($("#Software_Team,#Faculty_Advisors").hasClass("active")){
-				if ($(window).width() > 1350) {
-					$('.info').css({"height":"1100px"});
-				}
-				else if($(window).width() > 850){
-					$('.info').css({"height":"3000px"});
-				}
-				else{
-					$('.info').css({"height":"4050px"});
-					if($("#Software_Team").hasClass("active")){
-						$('.info').css({"height":"4850px"});
-					}
-					if($(window).width() < 700){
-						$('.info').css({"height":"1700px"});
-					}
-				}
-			}
-			else{
-				if ($(window).width() > 1350) {
-					$('.info').css({"height":"750px"});
-				}
-				else if($(window).width() > 850){
-					$('.info').css({"height":"750px"});
-				}
-				else{
+			//$(page[0]).addClass("active");
+			// if($("#Software_Team,#Faculty_Advisors").hasClass("active")){
+				// if ($(window).width() > 1350) {
+					// $('.info').css({"height":"1100px"});
+				// }
+				// else if($(window).width() > 850){
+					// $('.info').css({"height":"3000px"});
+				// }
+				// else{
+					// $('.info').css({"height":"4050px"});
+					// if($("#Software_Team").hasClass("active")){
+						// $('.info').css({"height":"4850px"});
+					// }
+					// if($(window).width() < 700){
+						// $('.info').css({"height":"1700px"});
+					// }
+				// }
+			// }
+			// else{
+				// if ($(window).width() > 1350) {
+					// $('.info').css({"height":"750px"});
+				// }
+				// else if($(window).width() > 850){
+					// $('.info').css({"height":"750px"});
+				// }
+				// else{
 
-					$('.info').css({"height":"2300px"});
-					if($('#Mechanical_Team').hasClass("active")){	
-						$('.info').css({"height":"3050px"});
-					}
-					if($(window).width() < 700){
-						$('.info').css({"height":"1500px"});
-					}
-				}
-			}
+					// $('.info').css({"height":"2300px"});
+					// if($('#Mechanical_Team').hasClass("active")){	
+						// $('.info').css({"height":"3050px"});
+					// }
+					// if($(window).width() < 700){
+						// $('.info').css({"height":"1500px"});
+					// }
+				// }
+			// }
 		}
 		
 		 
