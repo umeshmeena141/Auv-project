192d9a8e92d8acf584ea4d4a4d838244f6061f73
Umesh Meena <umeshmeena141@gmail.com>
2017-12-23 17:07:57 +0530

    Email

--- Gemfile
+++ Gemfile
@@ -7,4 +7,5 @@ group :jekyll_plugins do
   gem 'jekyll'
   gem "jekyll-gist"
 
+
 end
--- _pages/email.php
+++ _pages/email.php
@@ -1,8 +1,8 @@
-%raw%<?php
+<?php
   $name=$_POST['name'];
   $email=$_POST['email'];
   $msg=$_POST['msg'];
   echo "Done"
   $message="Name : " + $name + "    Email : " + $email + "     Message : " + $msg ;
   mail("umeshmeena141@gmail.com","newmsg",$message);
- ?>% endraw %
+ ?>
