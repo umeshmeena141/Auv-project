4116e3ff469b62cbf86c80e424a1a06c9dfc7190
Umesh Meena <umeshmeena141@gmail.com>
2017-12-23 15:54:04 +0530

    Changed contact page

--- Gemfile
+++ Gemfile
@@ -1,13 +1,10 @@
 source 'https://rubygems.org'
 
 group :jekyll_plugins do
-  gem "github-pages"
   gem "jekyll-paginate"
   gem "jekyll-feed"
   gem "rake"
   gem 'jekyll'
   gem "jekyll-gist"
-  gem 'jemoji'
-  gem "html-proofer"
 
 end
---
+++ _pages/email.php
@@ -0,0 +1,8 @@
+%raw%<?php
+  $name=$_POST['name'];
+  $email=$_POST['email'];
+  $msg=$_POST['msg'];
+  echo "Done"
+  $message="Name : " + $name + "    Email : " + $email + "     Message : " + $msg ;
+  mail("umeshmeena141@gmail.com","newmsg",$message);
+ ?>% endraw %
