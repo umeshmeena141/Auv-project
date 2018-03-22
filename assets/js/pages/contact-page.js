c0664c3ae2381149d71c5d111757998f502a2690
umeshmeena141 <umeshmeena141@gmail.com>
2018-03-22 21:15:16 +0530

    new changes

--- assets/js/pages/contact-page.js
+++ assets/js/pages/contact-page.js
@@ -9,37 +9,13 @@ $(document).ready(function() {
       $('#page_heading').css({
         "fontSize": "35px"
       });
-      $('.contactdetails').css({
-        "padding-left": "0% !important"
-      });
-      $('#sidebar').css({
-        "padding-left": "0% !important"
-      });
       $('.col-md-5').css({
         "margin-left": "0% !important"
       });
       $('h1').css({
         "font-size": "30px"
       });
-      $('.contactblocks').css({
-        "height": "auto"
-      })
-
-    }
-    if ($(window).width() < 390) {
-      $('.icons').css({
-        "padding-top": "4%"
-      });
-      $('.socialicons').css({
-        "text-indent": "15%"
-      });
-      $('.auvcontact').css({
-        "padding-left": "12% !important"
-      });
-    } else {
-      $('.contactblocks').css({
-        "height": "300px"
-      })
     }
   });
+
 });
